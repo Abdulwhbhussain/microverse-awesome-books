ccfb12fb512942447939dc734656c39a5323a3d5
Fezzopro <fezzopro@gmail.com>
2023-04-24 19:29:45 +0200

    Correct Eslint errors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // A collection that keeps a list of books.
   const bookList = document.querySelector('#book-list');
   let collectionOfBooks = [];
+
   function bookOnHtmlPage(books) {
     books.forEach((book, id) => {
       const bookItem = document.createElement('li');
       bookItem.setAttribute('id', `book-item-${id}`);
-      bookItem.setAttribute('class', `book-item`);
+      bookItem.setAttribute('class', 'book-item');
       bookItem.innerHTML = `<div>
     <h3>${book.title}</h3>
     <p>${book.author}</p>
@@ -15,7 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
     </div>`;
       bookList.appendChild(bookItem);
     });
-  };
+  }
+
   // Remove a book from the collection.
   function removeFunction() {
     if (collectionOfBooks.length > 0) {
@@ -33,11 +35,13 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   }
+
   function bookOnHtmlPageRemove() {
-    document.querySelectorAll('.book-item').forEach((book, id) => {
+    document.querySelectorAll('.book-item').forEach((book) => {
       book.remove();
     });
-  };
+  }
+
   if (!localStorage.getItem('collectionOfBooks')) {
     localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
   } else {
@@ -45,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     bookOnHtmlPage(collectionOfBooks);
   }
   removeFunction();
+
   // Add a new book to the collection with title and author.
   document.querySelector('#submit-btn').addEventListener('click', (e) => {
     e.preventDefault();
