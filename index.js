40837dad490852e7ce187a96444c9decb502b2f6
Fezzopro <fezzopro@gmail.com>
2023-04-24 18:31:40 +0200

    Refactoring index.js codes

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
-// A collection that keeps a list of books.
-const bookList = document.querySelector('#book-list');
-let collectionOfBooks = [];
+document.addEventListener('', () => {
+  // A collection that keeps a list of books.
+  const bookList = document.querySelector('#book-list');
+  let collectionOfBooks = [];
+  if (!localStorage.getItem('collectionOfBooks')) {
+    localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
+  } else {
+    collectionOfBooks = JSON.parse(localStorage.getItem('collectionOfBooks'));
+    bookOnHtmlPage(collectionOfBooks);
+  }
 
-if (!localStorage.getItem('collectionOfBooks')) {
-  localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
-} else {
-  collectionOfBooks = JSON.parse(localStorage.getItem('collectionOfBooks'));
-  bookOnHtmlPage(collectionOfBooks);
-}
+    removeFunction();
 
-removeFunction();
-
-function bookOnHtmlPage(books) {
-  books.forEach((book, id) => {
+  function bookOnHtmlPage(books) {
+    books.forEach((book, id) => {
       const bookItem = document.createElement('li');
       bookItem.setAttribute('id', `book-item-${id}`);
-      bookItem.setAttribute('class', `book-item`);
+      bookItem.setAttribute('class', 'book-item');
       bookItem.innerHTML = `<center>
-      <h3>${book.title}</h3>
-      <p>${book.author}</p>
-      <button id=remove-btn-${id} class='remove-btn'>Remove</button>
-      <hr style='width: 50%;'>
-      </center>`;
+    <h3>${book.title}</h3>
+    <p>${book.author}</p>
+    <button id=remove-btn-${id} class='remove-btn'>Remove</button>
+    <hr style='width: 50%;'>
+    </center>`;
       bookList.appendChild(bookItem);
-  });
-};
-
-// Add a new book to the collection with title and author.
-
-document.querySelector('#submit-btn').addEventListener('click', (e) => {
-  e.preventDefault();
-  const title = document.querySelector('#title').value;
-  document.querySelector('#title').value = '';
-  const author = document.querySelector('#author').value;
-  document.querySelector('#author').value = '';
-  collectionOfBooks.push({ title, author });
-  localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
-  bookOnHtmlPageRemove();
-  bookOnHtmlPage(collectionOfBooks);
-  removeFunction();
-});
-
-// Remove a book from the collection.
-
-function removeFunction() {
-  if (collectionOfBooks.length > 0) {
-    collectionOfBooks.forEach((book, id) => {
-      document.querySelector(`#remove-btn-${id}`).addEventListener('click', (e) => {
-        e.preventDefault();
-        if (collectionOfBooks.length === 1) {
-            collectionOfBooks.pop();
-        } else {
-            collectionOfBooks.splice(id, 1);
-        }
-        localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
-        document.querySelector(`#book-item-${id}`).remove();
-      });
     });
   }
-}
 
-function bookOnHtmlPageRemove() {
-  document.querySelectorAll('.book-item').forEach((book, id) => {
-    book.remove();
+  const bookOnHtmlPage = () => {
+    document.querySelectorAll('.book-item').forEach((book, id) => {
+      book.remove();
+    });
+  };
+
+    // Add a new book to the collection with title and author.
+
+  document.querySelector('#submit-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = document.querySelector('#title').value;
+    document.querySelector('#title').value = '';
+    const author = document.querySelector('#author').value;
+    document.querySelector('#author').value = '';
+    collectionOfBooks.push({ title, author });
+    localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
+    bookOnHtmlPageRemove();
+    bookOnHtmlPage(collectionOfBooks);
+    removeFunction();
   });
-};
 
+    // Remove a book from the collection.
+
+    function removeFunction() {
+    if (collectionOfBooks.length > 0) {
+      collectionOfBooks.forEach((book, id) => {
+        document.querySelector(`#remove-btn-${id}`).addEventListener('click', (e) => {
+          e.preventDefault();
+          if (collectionOfBooks.length === 1) {
+            collectionOfBooks.pop();
+          } else {
+            collectionOfBooks.splice(id, 1);
+          }
+          localStorage.setItem('collectionOfBooks', JSON.stringify(collectionOfBooks));
+          document.querySelector(`#book-item-${id}`).remove();
+        });
+      });
+    }
+  }
+});
